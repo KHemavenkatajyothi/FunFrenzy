5e0cc5993fff105e6949679baad8184fa01d9cf8
KHemavenkatajyothi <20pa1a1248@vishnu.edu.in>
2023-04-27 09:10:42 +0530

    second commit

--- routes/app.js
+++ routes/app.js
@@ -6,6 +6,11 @@ const ejs=require('ejs');
 // var cString=""
 // var db = mongojs(cString, [])
 
+// router.use(
+//     express.urlencoded({
+//       extended: true
+//     })
+// )
 var mongojs = require('mongojs')
 var cString="mongodb+srv://Hema:Hema@cluster0.r5stcec.mongodb.net/FunFrenzy?retryWrites=true&w=majority";
 var db = mongojs(cString, ['StoryTellers'])
@@ -70,6 +75,7 @@ router.post('/ccomitform',function(req,res){
         date:req.body.date2,
         place:req.body.place2,
         link:req.body.link2,
+        title:req.body.title,
     }
     console.log(data)
     db.commitments.insert(data,function(err,docs){
@@ -93,7 +99,7 @@ router.post('/page1',function(req,res){
     if(id3=="artist"){
         let promiseOfFind = test.find({name:id1,password:id2}).toArray((err, docs) => {
             if(docs.length!=0){
-                res.render("page1ar");
+                res.render("page1ar",{id3:id3,id1:id1});
             }
             else{
                 res.send("Enter valid details");
@@ -103,13 +109,19 @@ router.post('/page1',function(req,res){
     if(id3=="audience"){
         let promiseOfFind = test1.find({name:id1,password:id2}).toArray((err, docs1) => {
             if(docs1.length!=0){
-                res.render("page1");
+                res.render("page1",{id3:id3,id1:id1});
             }
             else{
                 res.send("enter valid details");
             }
         })
     }
+})
+router.get('/buyform',(req,res,next)=>{
+    res.render('buyform');
+})
+router.get('/sbuyform',(req,res,next)=>{
+    res.render('sbuyform');
 })
 // router.get('/stryar1',function(req,res){
 //     var test1=db.commitments;
@@ -120,62 +132,171 @@ router.post('/page1',function(req,res){
 //         }
 //     })
 // })
-router.get('/stry1',function(req,res){
+// router.get('/stry1',function(req,res){
+//     var test1=db.select;
+//     let promiseOfFind = test1.find({select:"storyteller"}).toArray((err, docs1) => {
+//         if(docs1.length!=0){
+//             console.log(docs1);
+//             res.render("stry1",{l1:docs1});
+//         }
+//     })
+// })
+// router.get('/dancer',function(req,res){
+//     var test1=db.select;
+//     let promiseOfFind = test1.find({select:"dancer"}).toArray((err, docs1) => {
+//         if(docs1.length!=0){
+//             console.log(docs1);
+//             res.render("dancer",{l1:docs1});
+//         }
+//     })
+// })
+// router.get('/pop',function(req,res){
+//     var test1=db.select;
+//     let promiseOfFind = test1.find({select:"popstar"}).toArray((err, docs1) => {
+//         if(docs1.length!=0){
+//             console.log(docs1);
+//             res.render("pop",{l1:docs1});
+//         }
+//     })
+// })
+// router.get('/stc',function(req,res){
+//     var test1=db.select;
+//     let promiseOfFind = test1.find({select:"standupcomedy"}).toArray((err, docs1) => {
+//         if(docs1.length!=0){
+//             console.log(docs1);
+//             res.render("stc",{l1:docs1});
+//         }
+//     })
+// })
+// router.get('/sing',function(req,res){
+//     var test1=db.select;
+//     let promiseOfFind = test1.find({select:"singer"}).toArray((err, docs1) => {
+//         if(docs1.length!=0){
+//             console.log(docs1);
+//             res.render("sing",{l1:docs1});
+//         }
+//     })
+// })
+// router.post('/page2',async function(req,res){
+//     console.log(id1);
+//     var value=req.body.val
+//     var test1=db.select;
+//     let promiseOfFind = test1.find({select:value}).toArray((err, docs1) => {
+//         if(docs1.length!=0){
+//             console.log(docs1);
+//             res.render("page2",{l1:docs1});
+//         }
+//     })
+// })
+router.post('/page2',async function(req,res){
+    console.log(id1);
+    var value=req.body.val;
+    var id3=req.body.rad;
+    var id1=req.body.id1;
     var test1=db.select;
-    let promiseOfFind = test1.find({select:"storyteller"}).toArray((err, docs1) => {
+    let promiseOfFind = test1.find({select:value}).toArray((err, docs1) => {
         if(docs1.length!=0){
             console.log(docs1);
-            res.render("stry1",{l1:docs1});
-        }
-    })
-})
-router.get('/dancer',function(req,res){
-    var test1=db.select;
-    let promiseOfFind = test1.find({select:"dancer"}).toArray((err, docs1) => {
-        if(docs1.length!=0){
-            console.log(docs1);
-            res.render("dancer",{l1:docs1});
-        }
-    })
-})
-router.get('/pop',function(req,res){
-    var test1=db.select;
-    let promiseOfFind = test1.find({select:"popstar"}).toArray((err, docs1) => {
-        if(docs1.length!=0){
-            console.log(docs1);
-            res.render("pop",{l1:docs1});
-        }
-    })
-})
-router.get('/stc',function(req,res){
-    var test1=db.select;
-    let promiseOfFind = test1.find({select:"standupcomedy"}).toArray((err, docs1) => {
-        if(docs1.length!=0){
-            console.log(docs1);
-            res.render("stc",{l1:docs1});
-        }
-    })
-})
-router.get('/sing',function(req,res){
-    var test1=db.select;
-    let promiseOfFind = test1.find({select:"singer"}).toArray((err, docs1) => {
-        if(docs1.length!=0){
-            console.log(docs1);
-            res.render("sing",{l1:docs1});
+            res.render("page2",{l1:docs1,id3:id3,id1:id1});
         }
     })
 })
 router.post('/stry11',async function(req,res){
-    var id1=req.body.name123;
+    var id=req.body.name123;
     var test1=db.commitments;
-    let promiseOfFind = test1.find({name:id1}).toArray((err, docs1) => {
+    var id1=req.body.id1;
+    var id3=req.body.rad;
+    let promiseOfFind = test1.find({name:id}).toArray((err, docs1) => {
         if(docs1.length!=0){
             console.log(docs1);
-            res.render("stry11",{l9:docs1});
+            res.render("stry11",{l9:docs1,id1:id1,id3:id3,id:id});
         }
     })
+})
+router.post('/sing11',async function(req,res){
+    var id=req.body.name123;
+    var test1=db.commitments;
+    var id1=req.body.id1;
+    var id3=req.body.rad;
+    let promiseOfFind = test1.find({name:id}).toArray((err, docs1) => {
+        if(docs1.length!=0){
+            console.log(docs1);
+            res.render("sing11",{l9:docs1,id1:id1,id3:id3,id:id});
+        }
     })
-
+})
+router.post('/PP11',async function(req,res){
+    var id=req.body.name123;
+    var test1=db.commitments;
+    var id1=req.body.id1;
+    var id3=req.body.rad;
+    let promiseOfFind = test1.find({name:id}).toArray((err, docs1) => {
+        if(docs1.length!=0){
+            console.log(docs1);
+            res.render("PP11",{l9:docs1,id1:id1,id3:id3,id:id});
+        }
+    })
+})
+router.post('/TT11',async function(req,res){
+    var id=req.body.name123;
+    var test1=db.commitments;
+    var id1=req.body.id1;
+    var id3=req.body.rad;
+    let promiseOfFind = test1.find({name:id}).toArray((err, docs1) => {
+        if(docs1.length!=0){
+            console.log(docs1);
+            res.render("TT11",{l9:docs1,id1:id1,id3:id3,id:id});
+        }
+    })
+})
+router.post('/dance11',async function(req,res){
+    var id=req.body.name123;
+    var test1=db.commitments;
+    var id1=req.body.id1;
+    var id3=req.body.rad;
+    let promiseOfFind = test1.find({name:id}).toArray((err, docs1) => {
+        if(docs1.length!=0){
+            console.log(docs1);
+            res.render("dance11",{l9:docs1,id1:id1,id3:id3,id:id});
+        }
+    })
+})
+router.post('/pop11',async function(req,res){
+    var id=req.body.name123;
+    var test1=db.commitments;
+    var id1=req.body.id1;
+    var id3=req.body.rad;
+    let promiseOfFind = test1.find({name:id}).toArray((err, docs1) => {
+        if(docs1.length!=0){
+            console.log(docs1);
+            res.render("pop11",{l9:docs1,id1:id1,id3:id3,id:id});
+        }
+    })
+})
+router.post('/stc11',async function(req,res){
+    var id=req.body.name123;
+    var test1=db.commitments;
+    var id1=req.body.id1;
+    var id3=req.body.rad;
+    let promiseOfFind = test1.find({name:id}).toArray((err, docs1) => {
+        if(docs1.length!=0){
+            console.log(docs1);
+            res.render("stc11",{l9:docs1,id1:id1,id3:id3,id:id});
+        }
+    })
+})
+router.post('/pt11',async function(req,res){
+    var id=req.body.name123;
+    var test1=db.commitments;
+    var id1=req.body.id1;
+    var id3=req.body.rad;
+    let promiseOfFind = test1.find({name:id}).toArray((err, docs1) => {
+        if(docs1.length!=0){
+            console.log(docs1);
+            res.render("pt11",{l9:docs1,id1:id1,id3:id3,id:id});
+        }
+    })
+})
 router.get('/TT',function(req,res){
     var test1=db.select;
     let promiseOfFind = test1.find({select:"tedtalk"}).toArray((err, docs1) => {
@@ -183,32 +304,74 @@ router.get('/TT',function(req,res){
             console.log(docs1);
             res.render("TT",{l1:docs1});
         }
-    
     })
 })
-router.get('/PP',function(req,res){
-    var test1=db.select;
-    let promiseOfFind = test1.find({select:"puppet"}).toArray((err, docs1) => {
-        if(docs1.length!=0){
-            console.log(docs1);
-            res.render("PP",{l1:docs1});
+router.post('/favs',function(req,res){
+    // console.log(req.body.id1);
+    // console.log(req.body.rad);
+    // console.log(req.body.id);
+    var id3=req.body.rad;
+    var user1={
+        u_name:req.body.id1,
+        fav_art_name:req.body.id,
+    }
+    let promiseOfFind = favorites.find({fav_art_name:id}).toArray(async(err, docs1) => {
+        if(await docs1.length==0){
+            db.favorites.insert(user1,function(err,docs){
+                if(err){
+                    console.log(err)
+                }
+                else{
+                    console.log(docs)
+                    res.render("favs",{id3:id3});
+                }
+            })
         }
     })
 })
-router.get('/pt',function(req,res){
-    var test1=db.select;
-    let promiseOfFind = test1.find({select:"poet"}).toArray((err, docs1) => {
-        if(docs1.length!=0){
-            console.log(docs1);
-            res.render("pt",{l1:docs1});
-        }
-    })
-})
+// router.get('/PP',function(req,res){
+//     var test1=db.select;
+//     let promiseOfFind = test1.find({select:"puppet"}).toArray((err, docs1) => {
+//         if(docs1.length!=0){
+//             console.log(docs1);
+//             res.render("PP",{l1:docs1});
+//         }
+//     })
+// })
+// router.get('/pt',function(req,res){
+//     var test1=db.select;
+//     let promiseOfFind = test1.find({select:"poet"}).toArray((err, docs1) => {
+//         if(docs1.length!=0){
+//             console.log(docs1);
+//             res.render("pt",{l1:docs1});
+//         }
+//     })
+// })
+router.post('/favorites',function(req,res){
+    var id1=req.body.id1;
+    var favo=db.favorites;
+    var favo1=db.select;
+    let promiseOfFind =favo.find({u_name:id1}).toArray((err, docs8) => {
+                if(docs8.length!=0){
+                     let promiseOfFind1 =favo1.find({u_name:id1}).toArray((err, docs7) => {
+                    //console.log(docs8);
+                    console.log(docs7);
+                    res.render("favorites",{l4:docs8,l5:docs7});
+                })
+                }
+            })
+        })
 router.get('/',function (req,res,next){
     res.render('landing_page');
 })
 router.get('/landing_page',function (req,res,next){
     res.render('landing_page');
+})
+router.post('/contactartform',function (req,res,next){
+    res.render('contactartform');
+})
+router.get('/scontactartform',function (req,res,next){
+    res.render('scontactartform');
 })
 router.get('/signup',(req,res,next)=>{
     res.render('signup');
@@ -232,10 +395,13 @@ router.post('/ccomitform',(req,res,next)=>{
     res.render('ccomitform');
 })
 router.get('/page1',(req,res,next)=>{
-    res.render('page1ar');
+    res.render('page1');
 })
 router.get('/page1ar',(req,res,next)=>{
     res.render('page1ar');
+})
+router.get('/favorites',(req,res,next)=>{
+    res.render('favorites');
 })
 
 router.get('/arform',(req,res,next)=>{
